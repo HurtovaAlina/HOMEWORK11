c7539cb02c5c847f2fd13295d57dc450606ae0fc
Alina <hurtovaalina2912@gmail.com>
2021-04-07 20:51:51 +0300

    HOMEWORK11 version2

--- personalmodule.js
+++ personalmodule.js
@@ -7,12 +7,12 @@ module.exports.showGreeting = function(username){
     // console.log(hour);
     
     if (hour >=23 || hour <5) {
-        return "Good night!", username;
+        return "Good night! " + username;
     } else if (hour >=5 && hour<11){
-        return "Good morning!", username;
+        return "Good morning!" + username;
     } else if (hour >=11 && hour<17){
-        return "Good afternoon!", username;
+        return "Good afternoon!" + username;
     } else if (hour>=17 && hour<23){
-        return "Good evening!", username;
+        return "Good evening!" + username;
     };
 }
